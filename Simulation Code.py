8d92516b64d34e318f0777664a2492fdff542510
Yuki Ishihara <u7329434@anu.edu.au>
2025-05-11 22:47:34 +1000

    Added Parameters

--- Simulation Code.py
+++ Simulation Code.py
@@ -1,2 +1,9 @@
-print("Hello World")
-print("Hello World 2")
+#Ising Mode Simulation
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Size of Lattice (size x size)
+size = 10          
+#Temperature (Given in units of e/K)          
+T = 2.5                      
