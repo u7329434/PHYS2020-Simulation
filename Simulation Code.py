9b2789798a3aeaf5200fb9df7a62059f3ca9592e
Yuki Ishihara <u7329434@anu.edu.au>
2025-05-12 12:27:52 +1000

    Added probability of flipping when delta U  > 0

--- Simulation Code.py
+++ Simulation Code.py
@@ -1,18 +1,23 @@
 #Ising Mode Simulation
 
+#Importing necessary packages
 import numpy as np
 import matplotlib.pyplot as plt
+
 #Setting up Parameters
-# Size of Lattice (size x size)
+#Size of Lattice (size x size)
 size = 10         
-#Temperature (Given in units of e/K)          
+#Temperature (K)         
 T = 2.5         
-#energy of neighboring dipoles, energy = -e when parallel and +e when parallel
+#Energy of neighboring dipoles, energy = -e when parallel and +e when parallel
 e = 1
+#Boltzmann Constant
+k = 1.38*10**(-23)
 
 #Initial lattice with each dipole randomly either having spin up or down
 lattice = np.random.choice([1, -1], size = (size, size))
 
+#Defining Delta U, returns U when the dipole is flipped - U when the dipole is not flipped
 def delta_U(i, j):
     center = lattice[i, j]
     center_changed = -lattice[i, j]
@@ -20,9 +25,12 @@ def delta_U(i, j):
     above = lattice[i, j + 1]
     left = lattice[i - 1, j]
     right = lattice[i + 1, j]
-    initial_U = -e*(center*below + center*above + center*left + center*right)
-    final_U = -e*(center_changed*below + center_changed*above + center_changed*left + center_changed*right)
-    return final_U - initial_U
+    not_changed_U = -e*(center*below + center*above + center*left + center*right)
+    changed_U = -e*(center_changed*below + center_changed*above + center_changed*left + center_changed*right)
+    return changed_U - not_changed_U
 
-
+#If delta U > 0 we flip the dipole with the following probability
+def prob_of_flipping(i, j):
+    prob = e^(-delta_U(i, j)/(kT))
+    return prob
 
